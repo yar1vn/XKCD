053f0f807b3580714b0d7638042cadf9ff8bcb4c
Yariv Nissim <yari.nis@me.com>
2020-01-20 21:36:27 -0800

    Initial Commit

---
+++ XKCD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XKCD
+//
+//  Created by Yariv on 1/20/20.
+//  Copyright © 2020 Yariv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
