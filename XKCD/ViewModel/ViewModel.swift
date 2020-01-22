18885720a474e35dd93474b79a58dd0518b8293c
Yariv Nissim <yari.nis@me.com>
2020-01-21 22:02:49 -0800

    old school collection view layout works better

--- XKCD/ViewModel/ViewModel.swift
+++ XKCD/ViewModel/ViewModel.swift
@@ -10,42 +10,46 @@ import UIKit
 
 // MARK: - View Model -
 
-final class ComicsViewModel {
-    enum Section {
-        case main
-    }
-
-    typealias ComicsDataSource = UICollectionViewDiffableDataSource<Section, Comic>
-    typealias ComicsSnapshot = NSDiffableDataSourceSnapshot<Section, Comic>
-
+final class ComicsViewModel: NSObject {
     private let service: XKCDServiceProtool
-    private(set) var numberOfComics = 0
+    private(set) var maximumNumberOfComics = 0
+    private var comics: [Comic] = []
 
     init(service: XKCDServiceProtool = XKCDService()) {
         self.service = service
     }
 
-    func setup(collectionView: UICollectionView, completion: @escaping (Result<ComicsDataSource, Error>) -> Void) {
+    func setup(collectionView: UICollectionView, completion: @escaping (Result<Void, Error>) -> Void) {
+        collectionView.dataSource = self
         collectionView.register(ComicCell.self, forCellWithReuseIdentifier: ComicCell.reuseIdentifier)
 
-        service.getLatestComic { result in
+        service.getLatestComic { [weak self] result in
+            guard let self = self else { return }
+
             do {
                 let comic = try result.get()
-                self.numberOfComics = comic.num
-
-                let dataSource = ComicsDataSource(collectionView: collectionView) { (collectionView, indexPath, comic) in
-                    collectionView.dequeueReusableCell(type: ComicCell.self, for: indexPath)?.configure(comic)
+                self.maximumNumberOfComics = comic.num // latest comic num is the max
+                self.comics = [comic]
+                
+                DispatchQueue.main.async {
+                    collectionView.reloadData()
                 }
-                var snapshot = ComicsSnapshot()
-                snapshot.appendSections([.main])
-                snapshot.appendItems([comic])
-                dataSource.apply(snapshot, animatingDifferences: false) {
-                    completion(.success(dataSource))
-                }
+                completion(.success(()))
             } catch {
                 completion(.failure(error))
             }
         }
+    }
+}
+
+extension ComicsViewModel: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return comics.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let comic = self.comics[indexPath.row]
+        return collectionView.dequeueReusableCell(type: ComicCell.self, for: indexPath)?.configure(comic) ?? UICollectionViewCell()
     }
 }
 
